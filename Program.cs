9d7785a86d3f4a093a76f4ffd6e09bdaa1163e2a
Hongfu Wang <hongwan@microsoft.com>
2019-07-23 10:09:24 +0800

    防止token过期导致接口调用失败

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,66 +24,16 @@ namespace AdfsSetbusinessUnit
 
 
             Console.WriteLine("Hello World!");
-            var client = new RestClient("https://adfs.demo.local/adfs/oauth2/token");
-            var request = new RestRequest(Method.POST);
 
-            request.AddHeader("Accept", "*/*");
-            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            request.AddParameter("undefined", "client_id=4d482433-0b9f-4082-9837-b745c8ce9e5d&client_secret=nETud9tWXmAno1q6_UOEKNOuQLwWk1etFJVTK3SW&resource=https%3A%2F%2Fcrm.demo.local%3A5555%2Fapi%2Fdata%2Fv9.0%2F&username=demo%5Cadministrator&password=&grant_type=password", ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
 
-            var obj = JsonConvert.DeserializeObject<adfsToken>(response.Content);
-
-            var access_token = obj.access_token;
 
             HttpClientHandler clientHandler = new HttpClientHandler();
             clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
 
-            // Pass the handler to httpclient(from you are calling api)
-            //var client = new HttpClient(clientHandler)
-            HttpClient _httpClient = new HttpClient(clientHandler);
-            _httpClient.BaseAddress = new Uri("https://crm.demo.local:5555/");
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _httpClient.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
-            _httpClient.DefaultRequestHeaders.Add("OData-Version", "4.0");
-            Console.WriteLine("input username：");
-            var us = Console.ReadLine(); 
-            Console.WriteLine("input password：");
-            var pwd = Console.ReadLine();
-            var content = new FormUrlEncodedContent(new[] {
-                new KeyValuePair<string,string>("client_id","4d482433-0b9f-4082-9837-b745c8ce9e5d"),
-                new KeyValuePair<string,string>("client_secret","nETud9tWXmAno1q6_UOEKNOuQLwWk1etFJVTK3SW"),
-                new KeyValuePair<string,string>("resource","https://crm.demo.local:5555/api/data/v9.0"),
-                new KeyValuePair<string,string>("username",us),
-               
-                new KeyValuePair<string,string>("password",pwd),
-                new KeyValuePair<string,string>("grant_type","password"),
-                         }); ; ;
-
-            var res = _httpClient.PostAsync("https://adfs.demo.local/adfs/oauth2/token", content);
-            var respo = res.Result.Content.ReadAsStringAsync().Result;
-            var accesstoken = JObject.Parse(respo).GetValue("access_token").ToString();
-
-            var myUri = new Uri("https://crm.demo.local:5555/api/data/v9.0/accounts?$top=1");
-            var myWebRequest = WebRequest.Create(myUri);
-            var myHttpWebRequest = (HttpWebRequest)myWebRequest;
-            myHttpWebRequest.PreAuthenticate = true;
-            myHttpWebRequest.Headers.Add("Authorization", "Bearer " + accesstoken);
-            myHttpWebRequest.Accept = "application/json";
-
-            var myWebResponse = myWebRequest.GetResponse();
-            var responseStream = myWebResponse.GetResponseStream();
-
-            var myStreamReader = new StreamReader(responseStream, Encoding.Default);
-            var json = myStreamReader.ReadToEnd();
-
-            responseStream.Close();
-            myWebResponse.Close();
-
-
-            HttpClient client2 = new HttpClient(clientHandler);
-
-            client2.DefaultRequestHeaders.Add("Authorization", "Bearer " + accesstoken);
+            var token = await GetToken();
+            HttpClient client2 =await InitHttpClient(token);
+        
+          
 
             var result = await client2.GetAsync("https://crm.demo.local:5555//api/data/v8.2/systemusers?$select=jobtitle,lf_id&$filter=isdisabled eq true").Result.Content.ReadAsStringAsync();
 
@@ -116,6 +67,12 @@ namespace AdfsSetbusinessUnit
 
             foreach (var item in listUser)
             {
+                if (token.expires_in_date <= DateTime.Now) {
+                    token = await GetToken();
+                    client2 = await InitHttpClient(token);
+                }
+                
+
                 Console.WriteLine($"set user userid:{item.systemuserid}, businessunitid:{item.jobtitle}");
                 //查询buid
                 var webURl = $"{baseUrl}businessunits?$select=lf_id&$filter=lf_id eq '{item.jobtitle}'";
@@ -125,7 +82,7 @@ namespace AdfsSetbusinessUnit
                 Console.WriteLine($"businessunitid:{buid}");
                 //启用用户
                 var systemuserUpdate = new systemuserUpdate() { isdisabled = false };
-                var content2 = new StringContent( JsonConvert.SerializeObject(systemuserUpdate) );
+                var content2 = new StringContent(JsonConvert.SerializeObject(systemuserUpdate));
                 content2.Headers.ContentType = new MediaTypeHeaderValue(@"application/json");
 
                 webURl = $"{baseUrl}systemusers({item.systemuserid})";
@@ -135,12 +92,12 @@ namespace AdfsSetbusinessUnit
 
                 //更改用户buid
                 webURl = $"{baseUrl}systemusers({item.systemuserid})/Microsoft.Dynamics.CRM.SetBusinessSystemUser()";
-                content2 = new StringContent("{\"BusinessUnit\":{\"businessunitid\":\""+buid+"\",\"@odata.type\":\"Microsoft.Dynamics.CRM.businessunit\"},\"ReassignPrincipal\":{\"systemuserid\":\""+item.systemuserid+"\",\"@odata.type\":\"Microsoft.Dynamics.CRM.systemuser\"}}");
+                content2 = new StringContent("{\"BusinessUnit\":{\"businessunitid\":\"" + buid + "\",\"@odata.type\":\"Microsoft.Dynamics.CRM.businessunit\"},\"ReassignPrincipal\":{\"systemuserid\":\"" + item.systemuserid + "\",\"@odata.type\":\"Microsoft.Dynamics.CRM.systemuser\"}}");
                 content2.Headers.ContentType = new MediaTypeHeaderValue(@"application/json");
 
 
-                message =await client2.PostAsync(webURl, content2);
-                
+                message = await client2.PostAsync(webURl, content2);
+
                 Console.WriteLine($"SetBusinessSystemUser PostAsync:{message.StatusCode} IsSuccessStatusCode:{message.IsSuccessStatusCode} ,count:{i}");
 
                 i++;
@@ -156,8 +113,58 @@ namespace AdfsSetbusinessUnit
         }
 
 
+        static async Task<adfsToken> GetToken() {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
 
+            // Pass the handler to httpclient(from you are calling api)
+            //var client = new HttpClient(clientHandler)
+            HttpClient _httpClient = new HttpClient(clientHandler);
+            _httpClient.BaseAddress = new Uri("https://crm.demo.local:5555/");
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+            _httpClient.DefaultRequestHeaders.Add("OData-Version", "4.0");
+            Console.WriteLine("input username：");
+            var us = Console.ReadLine();
+            Console.WriteLine("input password：");
+            var pwd = Console.ReadLine();
+            var content = new FormUrlEncodedContent(new[] {
+                new KeyValuePair<string,string>("client_id","4d482433-0b9f-4082-9837-b745c8ce9e5d"),
+                new KeyValuePair<string,string>("client_secret","nETud9tWXmAno1q6_UOEKNOuQLwWk1etFJVTK3SW"),
+                new KeyValuePair<string,string>("resource","https://crm.demo.local:5555/api/data/v9.0"),
+                new KeyValuePair<string,string>("username",us),
 
+                new KeyValuePair<string,string>("password",pwd),
+                new KeyValuePair<string,string>("grant_type","password"),
+                         }); ; ;
+
+            var res = await _httpClient.PostAsync("https://adfs.demo.local/adfs/oauth2/token", content);
+
+            var adfsToken = JsonConvert.DeserializeObject<adfsToken>(await res.Content.ReadAsStringAsync());
+
+            return adfsToken;
+        }
+
+        static async Task<HttpClient> InitHttpClient(adfsToken token)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client2 = new HttpClient(clientHandler);
+            
+            if (token.expires_in_date > DateTime.Now)
+            {
+                client2.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.access_token);
+            }
+            else
+            {
+                token = await GetToken();
+                client2 = new HttpClient(clientHandler);
+                client2.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.access_token);
+                
+            }
+
+            return client2;
+        }
 
     }
 
@@ -167,7 +174,21 @@ namespace AdfsSetbusinessUnit
         public string access_token { get; set; }
         public string token_type { get; set; }
 
-        public string expires_in { get; set; }
+
+        private string _expires_in;
+        public string expires_in
+        {
+            get
+            {
+                return _expires_in;
+            }
+            set
+            {
+                _expires_in = value;
+                this.expires_in_date = DateTime.Now.AddSeconds(int.Parse(value));
+
+            }
+        }
 
         public string resource { get; set; }
 
@@ -176,6 +197,9 @@ namespace AdfsSetbusinessUnit
         public string refresh_token_expires_in { get; set; }
 
         public string id_token { get; set; }
+
+
+        public DateTime expires_in_date { set; get; }
 
 
     }
